6d8cb474e6602057013d2b2bc383af487f9f9dd5
Jishu <Jishu@DESKTOP-J5EUKD0>
2022-08-12 16:15:41 +0530

    Commit 18

---
+++ collections/src/collections/collection2.java
@@ -0,0 +1,22 @@
+package collections;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class collection2 {
+
+	public static void main(String[] args) {
+		Collection<Integer> list = new ArrayList();
+
+		list.add(100);
+
+		list.add(10);
+
+		list.add(1000);
+
+
+		System.out.println("ArrayList : " + list);
+
+	}
+
+}
